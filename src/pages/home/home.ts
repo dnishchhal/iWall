dc0ebc77af4320ed7aa8859c1de4dc60400b01a3
Nishchhal Dixit <dnishchhal@gmail.com>
2018-02-05 01:29:35 +0530

    Pull Refresher Added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,12 @@
 import { Component,ViewChild } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { AngularFireStorage } from 'angularfire2/storage';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import {Content} from 'ionic-angular';
+import { File } from '@ionic-native/file';
+import {FileTransfer, FileTransferObject} from '@ionic-native/file-transfer'
+
 
 
 
@@ -13,37 +16,59 @@ import {Content} from 'ionic-angular';
 })
 
 
-
 export class HomePage {
-  //items: Observable<any[]>;
-
+  
   @ViewChild(Content)
     content:Content;
 
-  items = new Array();
+  defaultImage = 'assets/imgs/default.gif';
 
+  items = new Array();
+  names = new Array();
   imageChange$ = new Subject();
 
 
-  constructor(db: AngularFireStorage) {
-  	for (var i = 1; i <=33; i++) {
+  constructor(public transfer : FileTransfer, private file: File,db: AngularFireStorage, public toastCtrl: ToastController, 
+    public navCtrl : NavController) {
+  	
+    for (var i = 1; i <=33; i++) {
   		var a =   db.storage.ref().child(i%12+'.jpg');
+      this.names.push(i%12+'.jpg');
   		a.getDownloadURL()
   		.then(url => {
   			this.items.push(url);
   		});	
-
-  	}
-
-  	console.log(this.items);
-    
+  	}    
   }
+
+  fileTransfer: FileTransferObject = this.transfer.create();
 
   ngAfterViewInit() {
         this.content.ionScroll.subscribe((event: any) => {
-      console.log(event);
-      this.imageChange$.next();
+        	this.imageChange$.next();
     });
+    }
+
+    tap(id)
+    {
+        var url = id.target.id;
+        this.fileTransfer.download(url, this.file.dataDirectory + '1.jpg').then((entry) => {
+          console.log('download complete: ' + entry.toURL());
+        }, (error) => {
+          // handle error
+        });
+        this.toastCtrl.create({
+          message :  this.file.dataDirectory,
+          duration : 3000
+        }).present();
+    }
+
+     doRefresh(refresher) {
+      this.navCtrl.setRoot(this.navCtrl.getActive().component);
+      setTimeout(() => {
+        console.log('Async operation has ended');
+        refresher.complete();
+      }, 2000);
     }
 
 }
